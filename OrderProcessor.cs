3c741c723781f8e6a07b582a367211956d1b09cb
Giridhar Gopal <157975292+Giridhar706@users.noreply.github.com>
2026-04-23 14:50:22 +0530

    Initial Commit
    Added Order class and basic structure
    Implemented OrderProcessor with delegates and events
    Added Email, SMS, Logger services and completed program
    Added README and sample output

---
+++ OrderProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class OrderProcessor
+{
+    // Delegate
+    public delegate void OrderPlacedHandler(Order order);
+
+    // Event
+    public event OrderPlacedHandler OnOrderPlaced;
+
+    public void ProcessOrder(Order order)
+    {
+        Console.WriteLine("\nProcessing Order...");
+
+        // Trigger event
+        OnOrderPlaced?.Invoke(order);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Order ID: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Customer Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Amount: ");
+        double amount = Convert.ToDouble(Console.ReadLine());
+
+     
+        Order order = new Order
+        {
+            OrderId = id,
+            CustomerName = name,
+            Amount = amount
+        };
+
+        OrderProcessor processor = new OrderProcessor();
+
+      
+        EmailService email = new EmailService();
+        SMSService sms = new SMSService();
+        LoggerService logger = new LoggerService();
+
+     
+        processor.OnOrderPlaced += email.SendEmail;
+        processor.OnOrderPlaced += sms.SendSMS;
+        processor.OnOrderPlaced += logger.LogOrder;
+
+  
+        processor.ProcessOrder(order);
+
+        Console.ReadLine();
+    }
+}
